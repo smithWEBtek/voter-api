646c0152d8dbc96aee16670050625a9cfb41a47f
smithwebtek <brad@smithwebtek.com>
2019-01-20 18:47:56 -0500

    Edit voter model for potential nil value

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,6 +1,8 @@
 class Voter < ApplicationRecord
 
 	def address_string
-		self.street_number + ' ' +  self.street_name + ', ' + self.city + ', ' + self.state + ' ' + self.postal_code  
+		if self
+			self.street_number + ' ' +  self.street_name + ', ' + self.city + ', ' + self.state + ' ' + self.postal_code  
+ 		end
  	end
 end
