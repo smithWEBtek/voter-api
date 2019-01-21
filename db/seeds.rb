217a9ec7f4bfe734bd5c70b21f58aeff50b3e766
smithwebtek <brad@smithwebtek.com>
2019-01-21 05:47:29 -0500

    Update csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-def load_voters
-	csv_text = File.read(Rails.root.join('lib', 'voters.csv'))
+def load_voters1
+	csv_text = File.read(Rails.root.join('lib', 'voters1.csv'))
 	csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 	csv.each do |row|
 		voter = Voter.new(
@@ -9,6 +9,23 @@ def load_voters
 			city: row["city"],
 			state: row["state"],
 			postal_code: row["postal_code"]
+			)
+			voter.save
+			print '*'
+		end
+	end
+	
+	def load_voters2
+		csv_text = File.read(Rails.root.join('lib', 'voters2.csv'))
+		csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+		csv.each do |row|
+			voter = Voter.new(
+				vote_preference: row["vote_preference"].downcase,
+				street_number: row["street_number"],
+				street_name: row["street_name"],
+				city: row["city"],
+				state: row["state"],
+				postal_code: row["postal_code"]
 		)
 		voter.save
 		print '*'
@@ -23,7 +40,8 @@ end
 
 def main
 	truncate_database
-	load_voters
+	load_voters1
+	load_voters2
 end
 
 main
