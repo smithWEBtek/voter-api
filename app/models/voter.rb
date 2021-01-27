145d3940b97d94d59a6d9677bdc135c86662cdf5
brad@smithwebtek.com <brad@smithwebtek.com>
2021-01-26 20:21:03 -0500

    Prep for demo

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -18,8 +18,8 @@ class Voter < ApplicationRecord
 
 	def address_string
 		if self
-			"#{self.street_number} #{self.street_name}, #{self.city}, #{self.state} #{self.postal_code}"
 			# self.street_number + ' ' +  self.street_name + ', ' + self.city + ', ' + self.state + ' ' + self.postal_code  
+			"#{self.street_number} #{self.street_name}, #{self.city}, #{self.state} #{self.postal_code}"
  		end
  	end
 end
